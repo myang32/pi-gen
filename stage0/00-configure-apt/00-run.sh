d387d797487569ad25c7ffd54a6ab24b7619cf73
Stefan Scherer <scherer_stefan@icloud.com>
2017-07-22 13:18:40 +0200

    Revert key

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -2,7 +2,6 @@
 
 install -m 644 files/sources.list ${ROOTFS_DIR}/etc/apt/
 install -m 644 files/raspi.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
-[ "$IPOCUS_REPOS" = true ] && install -m 644 files/ipocus.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
 
 if [ -n "$APT_PROXY" ]; then
 	install -m 644 files/51cache ${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache
@@ -11,8 +10,7 @@ else
 	rm -f ${ROOTFS_DIR}/etc/apt/apt.conf.d/51cache
 fi
 
-on_chroot apt-key add - < files/ipocus.gpg.key
-[ "$IPOCUS_REPOS" = true ] && on_chroot apt-key add - < files/raspberrypi.gpg.key
+on_chroot apt-key add - < files/raspberrypi.gpg.key
 on_chroot << EOF
 apt-get update
 apt-get dist-upgrade -y
