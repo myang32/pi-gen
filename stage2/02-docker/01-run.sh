44a2a4d045fb539d86154c20203b0dec23eef642
Stefan Scherer <scherer_stefan@icloud.com>
2017-07-23 11:46:45 +0200

    Install debian stretch docker-ce 17.06.0

--- stage2/02-docker/01-run.sh
+++ stage2/02-docker/01-run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
 on_chroot << EOF
-curl -SsL https://get.docker.com | sh
+echo 'deb [arch=armhf] https://download.docker.com/linux/debian stretch edge' > /etc/apt/sources.list.d/docker.list
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-get install -y --no-install-recommends docker-ce
 pip install docker-compose
 EOF
