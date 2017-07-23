c6e2084a872c5afaf5df804c3d594bdefa0b812e
Stefan Scherer <scherer_stefan@icloud.com>
2017-07-23 12:20:30 +0200

    apt-get update

--- stage2/02-docker/01-run.sh
+++ stage2/02-docker/01-run.sh
@@ -3,6 +3,7 @@
 on_chroot << EOF
 echo 'deb [arch=armhf] https://download.docker.com/linux/debian stretch edge' > /etc/apt/sources.list.d/docker.list
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-get update
 apt-get install -y --no-install-recommends docker-ce
 pip install docker-compose
 EOF
